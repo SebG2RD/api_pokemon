1a18f3ec134b3a35edfb308cfd390b02ffcc64fb
SebG2RD <gerardsebastien@live.fr>
2025-11-04 15:05:19 +0100

    ajout du champs de recherche et amélioration des cards

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 const baseUrl = "https://tyradex.vercel.app/api/v1/pokemon";
 
 if (baseUrl) {
@@ -11,9 +12,9 @@ if (baseUrl) {
         return response.json();
       })
       .then((data) => {
-        console.log("Voici les données reçues :", data);
+        // console.log("Voici les données reçues :", data);
         if (Array.isArray(data)) {
-          data.slice(0, 20).forEach((item) => {
+          data.slice(0, 150).forEach((item) => {
             const pokemonCard = document.createElement("div");
             pokemonCard.classList.add("pokemon-card");
 
@@ -36,6 +37,35 @@ if (baseUrl) {
 
             const pokemonNameJapanese = document.createElement("p");
             pokemonNameJapanese.textContent = `Nom japonais : ${item.name.jp}`;
+
+            const pokemonSpritesContainer = document.createElement("div");
+            pokemonSpritesContainer.classList.add("pokemon-sprites-container");
+            const pokemonSprites = document.createElement("img");
+            pokemonSprites.classList.add("pokemon-sprites");
+            pokemonSprites.alt = item.name.fr;
+            pokemonSprites.src = item.sprites.regular;
+            pokemonSpritesShiny = document.createElement("img");
+            pokemonSpritesShiny.classList.add("pokemon-sprites-shiny");
+            pokemonSpritesShiny.alt = item.name.fr;
+            pokemonSpritesShiny.src = item.sprites.shiny;
+            let pokemonSpritesGmax = document.createElement("img");
+            pokemonSpritesGmax.classList.add("pokemon-sprites-gmax");
+            pokemonSpritesGmax.alt = item.name.fr;
+            if (item.sprites.gmax) {
+              if (
+                typeof item.sprites.gmax === "object" &&
+                item.sprites.gmax.regular
+              ) {
+                pokemonSpritesGmax.src = item.sprites.gmax.regular;
+              } else if (typeof item.sprites.gmax === "string") {
+                pokemonSpritesGmax.src = item.sprites.gmax;
+              } else {
+                pokemonSpritesGmax.src = "./assets/images/R.png";
+              }
+            } else {
+              pokemonSpritesGmax.src = "./assets/images/R.png";
+            }
+            console.log("pokemonSpritesGmax.src", pokemonSpritesGmax.src);
             container.appendChild(pokemonCard);
             pokemonCard.appendChild(pokemonName);
             pokemonCard.appendChild(pokemonImage);
@@ -43,6 +73,10 @@ if (baseUrl) {
             pokemonCard.appendChild(pokemonGeneration);
             pokemonCard.appendChild(pokemonNameEnglish);
             pokemonCard.appendChild(pokemonNameJapanese);
+            pokemonCard.appendChild(pokemonSpritesContainer);
+            pokemonSpritesContainer.appendChild(pokemonSprites);
+            pokemonSpritesContainer.appendChild(pokemonSpritesShiny);
+            pokemonSpritesContainer.appendChild(pokemonSpritesGmax);
           });
         } else {
           console.error("Erreur : les données reçues ne sont pas un tableau.");
@@ -55,3 +89,16 @@ if (baseUrl) {
     console.error("Erreur : le container n'a pas été trouvé.");
   }
 }
+
+const searchInput = document.getElementById("search");
+searchInput.addEventListener("input", (e) => {
+  const searchValue = e.target.value.toLowerCase();
+  const pokemonCards = document.querySelectorAll(".pokemon-card");
+  pokemonCards.forEach((pokemonCard) => {
+    if (pokemonCard.textContent.toLowerCase().indexOf(searchValue) !== -1) {
+      pokemonCard.style.display = "block";
+    } else {
+      pokemonCard.style.display = "none";
+    }
+  });
+});
